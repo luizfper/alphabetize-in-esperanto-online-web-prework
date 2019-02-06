7a41979a6d10a564a758bdde76499d667c5b1a7e
Luiz Pereira <luizfper@gmail.com>
2019-02-06 17:51:10 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,5 @@
 esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by{|a| a.split("").map {|char| esp_alph.index(char)}
+  arr.sort_by{|a| a.split("").map {|char| esp_alph.index(char)}}
 end
